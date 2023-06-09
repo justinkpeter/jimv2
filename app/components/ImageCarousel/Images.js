5934fe11d64f50370e5bd259a8a40bec495fa2a7
Justin <justinkmpeter@gmail.com>
2023-06-09 00:40:02 -0500

    portfolio images for ImageCarousel

---
+++ app/components/ImageCarousel/Images.js
@@ -0,0 +1,22 @@
+export const IMAGES = [
+    {
+        id: 0,
+        imageSrc: '/assets/img/IMG_8140.jpg'
+    },
+    {
+        id: 1,
+        imageSrc: '/assets/img/IMG_1006.jpg'
+    },
+    {
+        id: 2,
+        imageSrc: '/assets/img/IMG_8333.jpg'
+    },
+    {
+        id: 3,
+        imageSrc: '/assets/img/IMG_8212.jpg'
+    },
+    {
+        id: 4,
+        imageSrc: '/assets/img/IMG_0870.jpg'
+    }
+]
