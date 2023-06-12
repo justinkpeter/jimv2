4160b7014076d912afa81e69f7d116193e7a96cb
Justin <justinkmpeter@gmail.com>
2023-06-12 15:48:32 -0500

    updated carousel thumbnails

--- app/components/ImageCarousel/Images.js
+++ app/components/ImageCarousel/Images.js
@@ -1,22 +1,22 @@
 export const IMAGES = [
     {
         id: 0,
-        imageSrc: '/assets/img/IMG_8140.jpg'
+        imageSrc: '/assets/img/0/0.jpg'
     },
     {
         id: 1,
-        imageSrc: '/assets/img/IMG_1006.jpg'
+        imageSrc: '/assets/img/1/0.jpg'
     },
     {
         id: 2,
-        imageSrc: '/assets/img/IMG_8333.jpg'
+        imageSrc: '/assets/img/2/0.jpg'
     },
     {
         id: 3,
-        imageSrc: '/assets/img/IMG_8212.jpg'
+        imageSrc: '/assets/img/3/0.jpg'
     },
     {
         id: 4,
-        imageSrc: '/assets/img/IMG_0870.jpg'
+        imageSrc: '/assets/img/4/0.jpg'
     }
 ]
