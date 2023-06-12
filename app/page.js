17ff53afd9d9f8afde495f30db2832d4c62653fd
Justin <justinkmpeter@gmail.com>
2023-06-12 15:46:31 -0500

    framer shared layout

--- app/page.js
+++ app/page.js
@@ -1,37 +1,24 @@
-import Image from 'next/image'
+'use client'
+
 import { ImageCarousel } from "@/app/components/ImageCarousel/ImageCarousel";
 import { MagneticButton } from "@/app/components/MagneticButton/MagneticButton";
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
+import {motion} from "framer-motion";
 
 export default function Home() {
+
   return (
    <>
        <div className={'h-screen w-screen relative text-white font-clash '}>
            <ImageCarousel />
-           {/*sidebar*/}
-           <div className={'absolute inset-0'}>
-               <div className={'absolute w-[8%] border-r border-white h-screen backdrop-blur-2xl flex flex-col'}>
-                   <div className={'h-5/6 flex flex-col justify-center items-center border-b'}>
-                       <div className={'-rotate-90 uppercase text-center font-semibold '}>
-                           <div> "The Portfolio" </div>
-                           <div> justincrediblemoments </div>
-                       </div>
-                   </div>
-                   <div className={'h-1/6 flex flex-col justify-center items-center'}>
-                        <img className={'w-[15vh] h-[15vh] rounded-full object-cover'} src={'/assets/img/logo.png'}/>
-                   </div>
-               </div>
+           <div className={'absolute bottom-[25vh] sm:bottom-[10vh] left-0 sm:left-4 h-fit sm:ml-[8%] pl-[2%] font-semibold text-2xl sm:text-[5.5vw] uppercase leading-none '}>
+               <motion.h1 layoutId={'title'}> justincrediblemoments </motion.h1>
+               <h1> &quot;the portfolio&quot; </h1>
            </div>
-           <div className={'absolute bottom-[10vh]  h-fit ml-[8%] pl-[2%] font-semibold text-[5.5vw] uppercase leading-none '}>
-               <div> justincrediblemoments </div>
-               <div> "the portfolio" </div>
-           </div>
-           <div className={'absolute top-[8vh] font-medium w-[30vw] left-[10%] 2xl:w-[20vw] '}>
+           <div className={'absolute top-[8vh] font-medium sm:w-[30vw] w-[80vw]  sm:left-[10%] pl-2 sm:pl-4 2xl:w-[25vw] 2xl:text-3xl '}>
                <p>Photography is more than just “taking photos”,  but rather a ubiquitous art form, weaving creativity and wizardry into the fabric of our lives to create timeless moments.</p>
                 <div className={'py-16'}></div>
-
-               <MagneticButton Icon={ <ArrowRightIcon className={'w-10 h-10'}/> } />
-
+               <MagneticButton Icon={ <ArrowRightIcon className={'w-10 h-10'}/> } link={'/portfolio'}/>
            </div>
        </div>
    </>
